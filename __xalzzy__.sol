9b69d98df2d604a766d6928299785608f9d0bb46
Andrew Miracle <me@andrewmiracle.com>
2022-03-24 10:13:37 +0000

    intial commit with royalties

---
+++ __xalzzy__.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+import {ConfigSettings} from "gwei-slim-nft-contracts/contracts/base/ERC721Base.sol";
+import {ERC721Delegated} from "gwei-slim-nft-contracts/contracts/base/ERC721Delegated.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+
+contract __xalzzy__ is ERC721Delegated, ReentrancyGuard {
+  using Counters for Counters.Counter;
+
+  constructor(address baseFactory, string memory customBaseURI_)
+    ERC721Delegated(
+      baseFactory,
+      "__xalzzy__",
+      "0xa",
+      ConfigSettings({
+        royaltyBps: 2500,
+        uriBase: customBaseURI_,
+        uriExtension: "",
+        hasTransferHook: false
+      })
+    )
+  {}
+
+  /** MINTING **/
+
+  Counters.Counter private supplyCounter;
+
+  function mint() public nonReentrant onlyOwner {
+    require(saleIsActive, "Sale not active");
+
+    _mint(msg.sender, totalSupply());
+
+    supplyCounter.increment();
+  }
+
+  function totalSupply() public view returns (uint256) {
+    return supplyCounter.current();
+  }
+
+  /** ACTIVATION **/
+
+  bool public saleIsActive = true;
+
+  function setSaleIsActive(bool saleIsActive_) external onlyOwner {
+    saleIsActive = saleIsActive_;
+  }
+
+  /** URI HANDLING **/
+
+  function setBaseURI(string memory customBaseURI_) external onlyOwner {
+    _setBaseURI(customBaseURI_, "");
+  }
+
+  /** PAYOUT **/
+
+  function withdraw() public nonReentrant {
+    uint256 balance = address(this).balance;
+
+    Address.sendValue(payable(_owner()), balance);
+  }
+}
+
+// Contract created with Studio 721 v1.5.0
+// https://721.so
