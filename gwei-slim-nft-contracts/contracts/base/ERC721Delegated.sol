c5c4cc84b4182c8ebb8dcc363872209ae840dc81
Andrew Miracle <me@andrewmiracle.com>
2022-05-26 01:18:53 +0000

    allow solidity version upward compatibility

--- gwei-slim-nft-contracts/contracts/base/ERC721Delegated.sol
+++ gwei-slim-nft-contracts/contracts/base/ERC721Delegated.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.8.9;
+pragma solidity >=0.8.9;
 
 import {StorageSlotUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/StorageSlotUpgradeable.sol";
 
